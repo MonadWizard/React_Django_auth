abd322fcab4c51761b3761164658f467f1ba98d2
MonadWizard <monad.wizard.r@gmail.com>
2021-08-23 12:53:17 +0600

    update google social login

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-
 class UserAccountManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('User must have an email address')
-        
+            raise ValueError('Users must have an email address')
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
 
@@ -15,12 +14,8 @@ class UserAccountManager(BaseUserManager):
 
         return user
 
-    # def create_superuser():   # for super user
-
-
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
-    # name = models.CharField(max_length=255)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -36,9 +31,6 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     def get_short_name(self):
         return self.first_name
-
+    
     def __str__(self):
         return self.email
-
-
-
--- backend/manage.py
+++ backend/manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mail_auth_system.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth_system.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
